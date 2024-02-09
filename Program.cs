c25a8116485a0b3e113137f9a12d2022928ea87b
Kolcman <m.kolcman@yandex.ru>
2024-02-09 16:58:46 +0300

    Lesson3:Task1- add fix

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ if ((number >= 100) && (number < 1000))
     int figure1 = number / 100;
     int figure2 = number % 100 / 10;
     int figure3 = number % 10;
-    System.Console.WriteLine(figure1 + "" + figure3);
+    System.Console.WriteLine($"{figure1}{figure3}");
 }
 else
 {
