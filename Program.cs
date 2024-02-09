3f6fa99c766b8bdd4e3c76d9325d66fdbc691682
Kolcman <m.kolcman@yandex.ru>
2024-02-09 16:50:14 +0300

    Lesson3:Task1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿System.Console.WriteLine("Введите 3-х значное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number >= 100) && (number < 1000)) 
+{
+    int figure1 = number / 100;
+    int figure2 = number % 100 / 10;
+    int figure3 = number % 10;
+    System.Console.WriteLine(figure1 + "" + figure3);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не 3-х значное число!");
+}
